37dd836e43525e847d4c8a885ab5f1fc0606ead8
Szymon <szczesny.szymon.pl@gmail.com>
2020-05-12 09:44:23 +0200

    Added hours.
    This version isn't tested.

--- lampka.py
+++ lampka.py
@@ -6,8 +6,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(rel,GPIO.OUT, initial=GPIO.HIGH)
 
 while True:
-    czas = time.strftime("%S")
-    if int(czas) == 5 :
-        GPIO.output(rel, GPIO.LOW)
-        time.sleep(5)
-        GPIO.output(rel, GPIO.HIGH)
+    hours = time.strftime("%H")
+    seconds = time.strftime("%S")
+    if int(hours) == 10 :
+        if int(seconds) == 5:
+            GPIO.output(rel, GPIO.LOW)
+            time.sleep(5)
+            GPIO.output(rel, GPIO.HIGH)
