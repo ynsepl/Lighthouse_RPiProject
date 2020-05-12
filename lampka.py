4dcf29555716a75fc3d683aa56be3e1955026c27
Szymon <szczesny.szymon.pl@gmail.com>
2020-05-12 09:52:31 +0200

    Added mechanic start.
    Very big update. "s" to start "space" to stop.

--- lampka.py
+++ lampka.py
@@ -1,10 +1,13 @@
 import RPi.GPIO as GPIO
 import time
+import keyboard
+
 
 rel = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(rel,GPIO.OUT, initial=GPIO.HIGH)
 
+#Automatic start
 while True:
     hours = time.strftime("%H")
     seconds = time.strftime("%S")
@@ -13,3 +16,12 @@ while True:
             GPIO.output(rel, GPIO.LOW)
             time.sleep(5)
             GPIO.output(rel, GPIO.HIGH)
+while True:
+    try:  
+        if keyboard.is_pressed('s'):  
+            GPIO.output(rel, GPIO.LOW)
+            pass
+        
+        elif keyboard.is_pressed("space"):
+            GPIO.output(rel, GPIO.HIGH)
+            pass
