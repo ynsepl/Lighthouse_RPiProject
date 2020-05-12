baeee98073312dd8ada5e06459d0815986b14af6
Szymon <szczesny.szymon.pl@gmail.com>
2020-05-12 09:38:48 +0200

    Deleted datetime.

--- lampka.py
+++ lampka.py
@@ -1,14 +1,13 @@
 import RPi.GPIO as GPIO
 import time
-import datetime
 
 rel = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(rel,GPIO.OUT, initial=GPIO.HIGH)
+
 while True:
-    czas = datetime.datetime.now()
-    if czas.minute == 17 :
+    czas = time.strftime("%S")
+    if int(czas) == 5 :
         GPIO.output(rel, GPIO.LOW)
         time.sleep(5)
         GPIO.output(rel, GPIO.HIGH)
-        time.sleep(55)
